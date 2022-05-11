3f0851f0d36359b0ccc6e5f2c38d9552ad351e57
CappedMonke <100119945+CappedMonke@users.noreply.github.com>
2022-05-11 19:43:17 +0200

    reload

--- README.md
+++
@@ -1,7 +0,0 @@
-# Stick With It
-
-A small 2D-game made by a few SRH-Heidelberg students for educational purposes.
-
-## License
-
-See `LICENCE` file for details.
--- src/scenes/game_scene.cpp
+++ src/scenes/game_scene.cpp
@@ -1,4 +1,3 @@
-#include "game_scene.h"
 #include <memory>
 #include <string>
 
@@ -14,10 +13,10 @@ using namespace std::string_literals;
 
 game::scenes::GameScene::GameScene() {
     // Your scene initialization code here...
-    std::shared_ptr<game::core::Actor> actor_player = std::make_unique<game::core::Actor>(std::make_unique<game::core::Sprite>(std::make_shared<game::core::Texture2D>("assets/graphics/scarecrow.png"), 320 - 16, 180 - 16));
-    this->actors.insert(std::make_pair("actor1", actor_player));
+    std::shared_ptr<game::core::Actor> actor1 = std::make_unique<game::core::Actor>(std::make_unique<game::core::Sprite>(std::make_shared<game::core::Texture2D>("assets/graphics/ball.png"), 100, 100));
+    this->actors.insert(std::make_pair("actor1", actor1));
 
-    std::shared_ptr<game::core::Actor> actor2 = std::make_unique<game::core::Actor>(std::make_unique<game::core::SpriteAnimated>(std::make_shared<game::core::Texture2D>("assets/graphics/anim_sprite.png"), 80.0f, 80.0f, 1, 3, 50, 100, 200));
+    std::shared_ptr<game::core::Actor> actor2 = std::make_unique<game::core::Actor>(std::make_unique<game::core::SpriteAnimated>(std::make_shared<game::core::Texture2D>("assets/graphics/anim_sprite.png"), 80.0f, 80.0f, 1, 3, 50, 100, 300));
     this->actors.insert(std::make_pair("actor2", actor2));
 }
 
@@ -29,28 +28,6 @@ void game::scenes::GameScene::Update() {
     // Your process input and update game scene code here...
     if (IsKeyPressed(KEY_ESCAPE))
         game::core::Store::stage->switchToNewScene("pause"s, std::make_unique<PauseScene>());
-
-    std::shared_ptr<Vector2> velocity = std::make_shared<Vector2>(0, 0);
-
-    if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) 
-    {
-        velocity->x -= 32;
-    }
-    else if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT))
-    {
-        velocity->x += 32;
-    }
-    else if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))
-    {
-        velocity->y -= 32;
-    }
-    else if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
-    {
-        velocity->y += 32;
-    }
-
-    //actors.at("actor1")->sprite_->pos_x += velocity->x;
-    //actors.at("actor1")->sprite_->pos_y += velocity->y;
 }
 
 void game::scenes::GameScene::Draw() {
