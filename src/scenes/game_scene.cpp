d2c937ad6985ad2803155d6eb0263a1743235e7e
Silas Schürger <100119945+CappedMonke@users.noreply.github.com>
2022-05-28 15:27:05 +0200

    player movement
    added place-phase at beginning of level
    added limited moves per turn
    added place-phase-reset after new level
    
    NEEDS TO BE POLISHED SO BAD LUL

--- src/scenes/game_scene.cpp
+++ src/scenes/game_scene.cpp
@@ -31,14 +31,23 @@ void game::scenes::GameScene::Update() {
     if (IsKeyPressed(KEY_ESCAPE))
         game::core::Store::stage->switchToNewScene("pause"s, std::make_unique<PauseScene>());
 
-    actors.at("actor_player")->playerMovement();
+    //place player at round start, transparent
+    actors.at("actor_player")->placePlayer();
 
-    tilesetter->exchangeTile(actors.at("actor_player")->getActorPosition(), level);
+    //move player when keys pressed
+    actors.at("actor_player")->playerMovement(actors.at("actor_player")->isPlayerAllowedToMove());
+
+    //exchange tiles when player touches them
+    if (actors.at("actor_player")->getFirstMovementIsOver())
+    {
+        tilesetter->exchangeTile(actors.at("actor_player")->getActorPosition(), level);
+    }
 
     //TEMP press b and switch level
     if (IsKeyPressed(KEY_B) && level != 2)
     {
         level++;
+        actors.at("actor_player")->setFirstMovementIsOver(false);
     }
 }
 
@@ -47,5 +56,21 @@ void game::scenes::GameScene::Draw() {
     // Note that scene-actors are drawn automatically
     DrawText("This is the game scene - press ESCAPE for pause", 10, 10, 30, LIGHTGRAY);
 
+    //draw level
     tilesetter->drawTilemap(level);
+
+    //shows how many points player is allowed to move
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        actors.at("actor_player")->setFirstMovementIsOver(true);
+    }
+    if (actors.at("actor_player")->getFirstMovementIsOver())
+    {
+        DrawText(TextFormat("%i moves left", actors.at("actor_player")->getMovePoints()), 20, 20, 20, BLACK);
+    }
+    else
+    {
+        actors.at("actor_player")->setFirstMovementIsOver(false);
+        DrawText(TextFormat("Place Player and press enter", actors.at("actor_player")->getMovePoints()), 20, 20, 20, BLACK);
+    }
 }
