ddfbe0f4f3cfab9b8c1445ac38f14afb63163e0f
LOD-Zero <levin.theil@mailbox.org>
2022-08-31 16:39:39 +0200

    Option Menu fix

--- src/scenes/windows/settings_window.cpp
+++ src/scenes/windows/settings_window.cpp
@@ -1,13 +1,17 @@
 #include "settings_window.h"
 
 scenes::windows::SettingsWindow::SettingsWindow()
-	: SettingTexture(LoadTexture("assets/graphics/ui/Options/options_menu_sheet.png"))
 {
 }
 
 scenes::windows::SettingsWindow::~SettingsWindow()
 {
-	UnloadTexture(SettingTexture);
+	UnloadTexture(OptionTexture);
+	UnloadTexture(FullscreenTexture);
+	UnloadTexture(SoundTexture);
+	UnloadTexture(KeyTexture);
+	UnloadTexture(F_Texture);
+	UnloadTexture(M_Texture);
 }
 
 void scenes::windows::SettingsWindow::Update()
@@ -19,5 +23,10 @@ void scenes::windows::SettingsWindow::Update()
 
 void scenes::windows::SettingsWindow::Draw()
 {
-	DrawTexture(SettingTexture, 172, 41, WHITE);
+	DrawTexture(OptionTexture, 172, 41, WHITE);
+	DrawTexture(FullscreenTexture, 234, 150, WHITE);
+	DrawTexture(SoundTexture, 234, 204, WHITE);
+	DrawTexture(KeyTexture, 375, 196, WHITE);
+	DrawTexture(F_Texture, 385, 158, WHITE);
+	DrawTexture(M_Texture, 385, 203, WHITE);
 }
--- src/scenes/windows/settings_window.h
+++ src/scenes/windows/settings_window.h
@@ -14,6 +14,11 @@ namespace scenes::windows
 		void Update() override;
 		void Draw() override;
 
-		Texture2D SettingTexture;
+		Texture2D OptionTexture = LoadTexture("assets/graphics/ui/Options/options_background_sheet.png");
+		Texture2D FullscreenTexture = LoadTexture("assets/graphics/ui/Options/fullscreen.png");;
+		Texture2D SoundTexture = LoadTexture("assets/graphics/ui/Options/sound_text_sheet.png");;
+		Texture2D KeyTexture = LoadTexture("assets/graphics/ui/Options/key_sheet.png");;
+		Texture2D F_Texture = LoadTexture("assets/graphics/ui/Options/f_texture.png");;
+		Texture2D M_Texture = LoadTexture("assets/graphics/ui/Options/m_texture.png");;
 	};
 }
